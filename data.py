76ab69f1e16847ff75c2545bc9681226b11ec6b5
palmsi <sirapo.palm@gmail.com>
2016-08-24 13:23:17 +0700

    1

--- data.py
+++ data.py
@@ -2,11 +2,13 @@
 # @Author: palmsi
 # @Date:   2016-08-24 13:05:03
 # @Last Modified by:   palmsi
-# @Last Modified time: 2016-08-24 13:07:00
+# @Last Modified time: 2016-08-24 13:21:53
 
 def main():
 
     print("computer sci")
+    print("2559")
+    print("chiangmai")
 
 
 
