fa68999a023db394baa4bbb16cba2d015696393c
palmsi <sirapo.palm@gmail.com>
2016-08-24 13:20:54 +0700

    2

--- data.py
+++ data.py
@@ -2,12 +2,12 @@
 # @Author: palmsi
 # @Date:   2016-08-24 13:05:03
 # @Last Modified by:   palmsi
-# @Last Modified time: 2016-08-24 13:07:00
+# @Last Modified time: 2016-08-24 13:20:38
 
 def main():
 
     print("computer sci")
-
+    print("chiang mai university")
 
 
 if __name__ = '__main__':
