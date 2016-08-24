cfe8e63975b41f0ce0b48c27c12df3d2b33cad5e
palmsi <sirapo.palm@gmail.com>
2016-08-24 13:07:33 +0700

    24/8 13.07

---
+++ data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: palmsi
+# @Date:   2016-08-24 13:05:03
+# @Last Modified by:   palmsi
+# @Last Modified time: 2016-08-24 13:07:00
+
+def main():
+
+    print("computer sci")
+
+
+
+if __name__ = '__main__':
+    main()
